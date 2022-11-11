4ab06a8582a9b0b55f97503d559337c5db0c0fbe
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-11-12 02:03:40 +0600

    Basic java

--- basicjava/BasicJava.java
+++ basicjava/BasicJava.java
@@ -3,8 +3,9 @@ package basicjava;
 public class BasicJava {
 
     public static void main(String[] args) {
-        System.out.print("My name is Rayhan Khan.\nI am leatning Java.\n");
+        System.out.print("My name is Rayhan Khan.\nI am learning Java.\n");
         System.out.println("This my first Java program");
         
+
     }
 }
