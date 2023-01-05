06a56820f40cd407f623c71d86fb13b282ec2d30
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2023-01-06 02:09:30 +0600

    27. Remove Element

--- MainJava/src/mainjava/Problem_10_leet_27.java
+++ MainJava/src/mainjava/Problem_10_leet_27.java
@@ -4,9 +4,8 @@ public class Problem_10_leet_27 {
     public static void main(String[] args) {
 
         int[] arrays = { 0, 1, 2, 2, 3, 4, 5, 2, 6, 7 };
-        int lengthh = arrays.length;
         int element = 2;
-        lengthh = RemoveDuplicate(arrays, element);
+        int lengthh = RemoveDuplicate(arrays, element);
         for (int j = 0; j < lengthh; j++) {
             System.out.print(arrays[j] + " ");
         }
