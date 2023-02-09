69ffa6718692d4d76c56ff5a6d6f4c30abbb7a1a
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2023-02-10 01:19:39 +0600

    Method overriding & constructor

---
+++ Dynamic method dispatch/Apply method overriding/Figure.java
@@ -0,0 +1,14 @@
+class Figure{
+    double length;
+    double width;
+
+    Figure(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+    //declared method
+    double area(){
+        System.out.println("Area of figure is undefined");
+        return 0;
+    }
+}
